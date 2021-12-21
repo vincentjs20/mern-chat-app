b4238a9e0b4587c56672f7979b72752cdcd5ad01
Vincent J. Sinatra <vincentyoelsinatra2@gmail.com>
2021-12-21 20:40:40 +0700

    [ADD] import axios, fetch data from backend

--- frontend/src/Pages/ChatPage.js
+++ frontend/src/Pages/ChatPage.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const ChatPage = () => {
+    const [chats, setChats] = useState([]);
+    const fetchChats = async() => {
+        const { data } = await axios.get('/api/chat');
+
+        setChats(data);
+    };
+
+    useEffect(() => {
+        fetchChats();
+    }, [])
     return (
         <div>
-            Chat Page
+            {chats.map(chat=>
+            <div key={chat._id}>
+                {chat.chatName}
+            </div>)
+            }
         </div>
     )
 }
