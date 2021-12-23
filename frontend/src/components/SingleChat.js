5ef478e6a134ece766e8528f6760228dc9a531a7
Vincent J. Sinatra <vincentyoelsinatra2@gmail.com>
2021-12-24 01:10:30 +0700

    [UPDATE] heroku script to prod

--- frontend/src/components/SingleChat.js
+++ frontend/src/components/SingleChat.js
@@ -15,7 +15,7 @@ import animationData from "../animations/typing.json";
 import io from "socket.io-client";
 import UpdateGroupChatModal from "./miscellaneous/UpdateGroupChatModal";
 import { ChatState } from "../Context/ChatProvider";
-const ENDPOINT = "http://localhost:5000"; // "https://vj-Chat-apps.herokuapp.com"; -> After deployment
+const ENDPOINT = "https://vj-chat-apps.herokuapp.com/"; // "https://vj-Chat-apps.herokuapp.com"; -> After deployment
 var socket, selectedChatCompare;
 
 const SingleChat = ({ fetchAgain, setFetchAgain }) => {
